9c5646e7eaa2e0dd5eaffb685ee652a51013b80f
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-03-21 14:04:50 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+//ADDITION
+var num1 = 2;
+var num2 = 3;
+var cart = num1 + num2;
+console.log(cart); //5
+//SUBTRACTION
+var num3 = 5;
+var num4 = 2;
+var total = num3 - num4;
+console.log(total); //3
+//MULTIPLICATION
+var bags = 4;
+var appels = 5;
+var totalappels = bags * appels;
+console.log(totalappels); //20
+//DIVIDE
+var totalgrapes = 20;
+var basket = 4;
+var eachbasket = totalgrapes / basket;
+console.log(eachbasket); //5
+//EXPONENTIATION
+var layers = 5;
+var banana = 5;
+var power = Math.pow(layers, 2);
+console.log(power); //25
+//MODULES
+var peach = 5;
+var purse = 2;
+var reminder = peach % purse;
+console.log(reminder); //1
+//UNARY OPERATOR
+var a = 5;
+var b = 2;
+++a;
+--b;
+console.log(a); // 6
+console.log(b); // 1
+//COMBINING OPERATORS
+var result = 3 + 4 * 5;
+console.log(result); // 23
---
+++ main.ts
@@ -0,0 +1,53 @@
+//ADDITION
+let num1: number = 2 ;
+let num2: number = 3;
+let cart: number = num1 + num2;
+console.log (cart) //5
+
+
+//SUBTRACTION
+let num3: number = 5;
+let num4: number = 2;
+let total: number = num3 - num4;
+console.log(total) //3
+
+
+//MULTIPLICATION
+let bags: number = 4;
+let appels: number = 5;
+let totalappels: number = bags * appels 
+console.log(totalappels) //20
+
+
+//DIVIDE
+let totalgrapes: number = 20;
+let basket: number = 4;
+let eachbasket: number = totalgrapes / basket 
+console.log(eachbasket) //5
+
+
+//EXPONENTIATION
+let layers: number = 5
+let banana: number = 5
+let power: number =layers ** 2
+console.log(power) //25
+
+
+//MODULES
+let peach = 5
+let purse = 2
+let reminder = peach % purse
+console.log(reminder)//1
+
+//UNARY OPERATOR
+let a = 5
+let b = 2
+++a
+--b
+console.log(a)// 6
+console.log(b)// 1
+
+
+//COMBINING OPERATORS
+let result = 3 + 4 * 5;
+console.log(result) // 23
